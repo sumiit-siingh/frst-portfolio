a5d0c91b30aaf9ddac558ded98d10b348fa6b117
Sumit Singh <sumitsinghsakarari@gmail.com>
2025-04-29 06:11:16 +0530

    updated resume link

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -28,7 +28,7 @@ export default function About() {
                     </div> */}
 
                     <div className="h-[40rem] w-full flex items-center justify-center">
-                        <PinContainer title="poke-me" href="https://purple-deloria-92.tiiny.site" target="_blank" rel="noopener noreferrer">
+                        <PinContainer title="poke-me" href="https://silver-natala-47.tiiny.site/" target="_blank" rel="noopener noreferrer">
                             <div className="flex basis-full flex-col p-4 tracking-tight text-slate-100/50 sm:basis-1/2 w-[20rem] h-[20rem]">
                                 <h3 className="max-w-xs pb-2 m-0 font-bold text-base text-slate-100">
                                     Resume Inside
