972a324f12322614d4d3f8b1e887bba311195703
Sumit Singh <sumitsinghsakarari@gmail.com>
2025-06-07 20:39:50 +0530

    added footer

---
+++ src/component/footer.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+export default function Footer (){
+    return (
+        <div className="bg-black text-center font-poppins-regular p-2 text-white opacity-85 ">
+            <div>Made with 💝 by <span className="font-beau-rivage-regular text-3xl text-red-400 font-semibold backdrop-blur-2xl">Sumit</span></div>
+        </div>
+    )
+}
--- src/layout.jsx
+++ src/layout.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Navbar from "./component/navbar";
+import Footer from "./component/footer";
 import {Outlet} from "react-router-dom"; 
 export default function Layout() {
   return (
     <div>
       <Navbar/>
       <Outlet/>
+      <Footer/>
     </div>
   );
 }
